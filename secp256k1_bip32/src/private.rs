82383753f5aacae947d195cb7e692965537d37f9
Yehor Popovych <yehor@crossroad.io>
2019-04-14 20:21:32 +0300

    Consts for memory layout in secp256k1

--- secp256k1_bip32/src/private.rs
+++ secp256k1_bip32/src/private.rs
@@ -14,8 +14,6 @@ use super::error::KeyError;
 use super::public::XPub;
 
 const HMAC_KEY: &[u8] = b"Bitcoin seed";
-const KEY_DATA_SIZE: usize = 77;
-const ENTROPY_SIZE: usize = 64;
 const BIP44_SOFT_UPPER_BOUND: u32 = 0x80000000;
 
 lazy_static! {
@@ -24,41 +22,71 @@ lazy_static! {
   ).unwrap();
 }
 
+mod data_layout {
+    pub const DEPTH_SIZE  : usize = 1;
+    pub const FINGERPRINT_SIZE : usize = 4;
+    pub const INDEX_SIZE   : usize = 4;
+    pub const CHAIN_CODE_SIZE : usize = 32;
+    pub const KEY_SIZE : usize = 32;
+    pub const CHECKSUM_SIZE : usize = 4;
+
+    pub const KEY_DATA_SIZE : usize = (
+      DEPTH_SIZE + FINGERPRINT_SIZE + INDEX_SIZE +
+      CHAIN_CODE_SIZE + KEY_SIZE + CHECKSUM_SIZE
+    );
+
+    pub const ENTROPY_SIZE: usize = CHAIN_CODE_SIZE + KEY_SIZE;
+
+    pub const DEPTH_START         : usize = 0;
+    pub const DEPTH_END           : usize = DEPTH_START + DEPTH_SIZE;
+    pub const FINGERPRINT_START   : usize = DEPTH_END;
+    pub const FINGERPRINT_END     : usize = FINGERPRINT_START + FINGERPRINT_SIZE;
+    pub const INDEX_START         : usize = FINGERPRINT_END;
+    pub const INDEX_END           : usize = INDEX_START + INDEX_SIZE;
+    pub const CHAIN_CODE_START    : usize = INDEX_END;
+    pub const CHAIN_CODE_END      : usize = CHAIN_CODE_START + CHAIN_CODE_SIZE;
+    pub const KEY_START           : usize = CHAIN_CODE_END;
+    pub const KEY_END             : usize = KEY_START + KEY_SIZE;
+    pub const CHECKSUM_START      : usize = KEY_END;
+    pub const CHECKSUM_END        : usize = CHECKSUM_START + CHECKSUM_SIZE;
+}
 
 pub struct XPrv {
   key: SecretKey,
-  chaincode: [u8; 32],
-  parent_fingerprint: [u8; 4],
+  chaincode: [u8; data_layout::CHAIN_CODE_SIZE],
+  parent_fingerprint: [u8; data_layout::FINGERPRINT_SIZE],
   depth: u8,
   index: u32
 }
 
 impl XPrv {
   pub fn from_data(data: &[u8]) -> Result<Self, KeyError> {
+    use self::data_layout::*;
+
     if data.len() != KEY_DATA_SIZE {
       return Err(KeyError::InvalidDataSize(data.len(), KEY_DATA_SIZE));
     }
 
     let mut sha256 = Sha256::new();
-    sha256.input(&data[0..72]);
+    sha256.input(&data[DEPTH_START..KEY_END]);
     let mut hash = [0u8; 32];
     sha256.result(&mut hash);
     sha256.reset();
     sha256.input(&hash);
     sha256.result(&mut hash);
 
-    if data[73..76] != hash[0..3] {
+    if data[CHECKSUM_START..CHECKSUM_END] != hash[0..CHECKSUM_SIZE] {
       return Err(KeyError::InvalidSecretKey);
     }
 
-    let depth: u8 = data[0];
-    let mut parent_fingerprint = [0u8; 4];
-    parent_fingerprint.copy_from_slice(&data[1..4]);
-    let index: u32 = BigEndian::read_u32(&data[5..8]);
-    let mut chaincode = [0u8; 32];
-    chaincode.copy_from_slice(&data[9..40]);
+    let depth: u8 = data[DEPTH_START];
+    let mut parent_fingerprint = [0u8; FINGERPRINT_SIZE];
+    parent_fingerprint.copy_from_slice(&data[FINGERPRINT_START..FINGERPRINT_END]);
+    let index: u32 = BigEndian::read_u32(&data[INDEX_START..INDEX_END]);
+    let mut chaincode = [0u8; CHAIN_CODE_SIZE];
+    chaincode.copy_from_slice(&data[CHAIN_CODE_START..CHAIN_CODE_END]);
 
-    SecretKey::parse_slice(&data[41..72])
+    SecretKey::parse_slice(&data[KEY_START..KEY_END])
       .map_err(|err| err.into())
       .and_then(|private_key| {
         let pub_key = PublicKey::from_secret_key(&private_key);
@@ -79,6 +107,8 @@ impl XPrv {
   }
 
   pub fn from_seed(seed: &bip39::Seed) -> Result<Self, KeyError> {
+    use self::data_layout::*;
+
     let mut hmac = Hmac::new(Sha512::new(), HMAC_KEY);
     hmac.input(seed.as_ref());
     let result = hmac.result();
@@ -87,10 +117,10 @@ impl XPrv {
       return Err(KeyError::InvalidEntropySize(entropy.len()));
     }
 
-    let mut i_l = [0u8; 32];
-    let mut i_r = [0u8; 32];
-    i_l.copy_from_slice(&entropy[0..31]);
-    i_r.copy_from_slice(&entropy[32..63]);
+    let mut i_l = [0u8; KEY_SIZE];
+    let mut i_r = [0u8; CHAIN_CODE_SIZE];
+    i_l.copy_from_slice(&entropy[0..KEY_SIZE]);
+    i_r.copy_from_slice(&entropy[KEY_SIZE..(KEY_SIZE+CHAIN_CODE_SIZE)]);
 
     SecretKey::parse(&i_l)
       .map_err(|err| err.into())
@@ -101,7 +131,7 @@ impl XPrv {
         }
         Ok(private_key)
       })
-      .map(|pk| Self { key: pk, chaincode: i_r, depth: 0, index: 0, parent_fingerprint: [0u8; 4] })
+      .map(|pk| Self { key: pk, chaincode: i_r, depth: 0, index: 0, parent_fingerprint: [0u8; FINGERPRINT_SIZE] })
   }
 
   pub fn public(&self) -> XPub {
@@ -109,6 +139,8 @@ impl XPrv {
   }
 
   pub fn serialize(&self) -> Vec<u8> {
+    use self::data_layout::*;
+
     let mut data = Vec::with_capacity(KEY_DATA_SIZE);
 
     data.push(self.depth);
@@ -125,7 +157,7 @@ impl XPrv {
     sha256.input(&hash);
     sha256.result(&mut hash);
 
-    data.extend_from_slice(&hash[0..3]);
+    data.extend_from_slice(&hash[0..CHECKSUM_SIZE]);
 
     data
   }
@@ -154,13 +186,15 @@ impl XPrv {
   }
 
   pub fn derive(&self, index: u32) -> Result<Self, KeyError> {
+    use self::data_layout::*;
+
     if self.depth == std::u8::MAX {
       return Err(KeyError::DeriveDepthTooBig);
     }
     let mut hmac = Hmac::new(Sha512::new(), &self.chaincode);
 
     if index >= BIP44_SOFT_UPPER_BOUND {
-      let mut input = Vec::with_capacity(util::SECRET_KEY_SIZE + 1 + 4);
+      let mut input = Vec::with_capacity(util::SECRET_KEY_SIZE + INDEX_SIZE + 1);
       input.push(0x00);
       input.extend_from_slice(&self.key.serialize());
       if input.write_u32::<BigEndian>(index).is_err() {
@@ -168,7 +202,7 @@ impl XPrv {
       }
       hmac.input(&input);
     } else {
-      let mut input = Vec::with_capacity(util::SECRET_KEY_SIZE + 4);
+      let mut input = Vec::with_capacity(util::SECRET_KEY_SIZE + INDEX_SIZE);
       input.extend_from_slice(&self.key.serialize());
       if input.write_u32::<BigEndian>(index).is_err() {
         return Err(KeyError::InternalError);
@@ -183,10 +217,10 @@ impl XPrv {
       return Err(KeyError::InvalidEntropySize(entropy.len()));
     }
 
-    let mut chaincode = [0u8; 32];
-    chaincode.copy_from_slice(&entropy[0..31]);
+    let mut chaincode = [0u8; CHAIN_CODE_SIZE];
+    chaincode.copy_from_slice(&entropy[0..CHAIN_CODE_SIZE]);
 
-    let bn = BigUint::from_bytes_be(&entropy[32..63]);
+    let bn = BigUint::from_bytes_be(&entropy[CHAIN_CODE_SIZE..(CHAIN_CODE_SIZE+KEY_SIZE)]);
     let curve_order = CURVE_ORDER.clone();
 
     if bn > curve_order {
@@ -227,8 +261,8 @@ impl XPrv {
       .map(|pk| {
         let mut hasher = Ripemd160::new();
         hasher.input(&self.public().sha256());
-        let mut fingerprint = [0u8; 4];
-        fingerprint.copy_from_slice(&hasher.result()[0..3]);
+        let mut fingerprint = [0u8; FINGERPRINT_SIZE];
+        fingerprint.copy_from_slice(&hasher.result()[0..FINGERPRINT_SIZE]);
 
         Self {
           key: pk,
